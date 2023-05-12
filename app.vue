757ca42d2e1cb805bd33d2e7a11e88e3e5b9ceea
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-12 23:33:42 +1000

    right comments

--- app.vue
+++ app.vue
@@ -11,7 +11,7 @@
     <div
       class="relative h-screen w-full bg-no-repeat bg-bottom bg-cover bg-[url('https://mysterio.yahoo.com/mysterio/api/8964DB5E909CACD18049306AFDB356D51F83B94D69A71CF8364374A44354E9D0/autoblog/resizefill_w1062_h597;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/os/ab/_cms/2021/10/12164830/2022-Lexus-LX-600-Ultra-Luxury-action-desert-front.jpg')]"
     >
-      <!-- -->
+      <!--home hero text -->
 
       <div class="z-10 absolute w-full h-full flex justify-center items-center">
         <div class="text-center w-1/2">
@@ -20,6 +20,7 @@
           </h1>
         </div>
       </div>
+      <!-- dark background -->
       <div class="z-1 absolute  w-full h-full bg-black opacity-50">
 
       </div>
