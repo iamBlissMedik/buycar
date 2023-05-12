acf6c48812b1443e1b546887309294db0d7e5e3f
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-12 23:19:13 +1000

    the navbar

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="text-orange-600">
-   <h1>hi</h1>
+  <div>
+    <!-- navbar -->
+    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+      <a class="text-3xl font-mono" href="">CarBuy</a>
+    </header>
   </div>
 </template>
