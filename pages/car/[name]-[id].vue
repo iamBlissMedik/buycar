59c1b3923f8fe30fa9bbf1543b03606f22eab3b3
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-13 06:35:35 +1000

    use composables

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,18 +1,18 @@
 <script setup>
 const route = useRoute();
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+const { capitalizeFirstLetter } = useUtilities();
 useHead({
   title: capitalizeFirstLetter(route.params.name),
+});
+definePageMeta({
+  layout: "custom",
 });
 </script>
 
 <template>
   <!-- car detail page -->
-  <div
-    class="mx-auto mt-32 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-  >
+
+  <div>
     <CarDetailsHero />
     <CarDetailsAttributes />
     <CarDetailsDescription />
--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -1,23 +1,20 @@
 <script setup>
 const route = useRoute();
-const capitalizeFirstLetter = (string) => {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-};
+const { capitalizeFirstLetter } = useUtilities();
 useHead({
   title: `${
     route.params.make ? capitalizeFirstLetter(route.params.make) : "Cars"
   } in ${capitalizeFirstLetter(route.params.city)}`,
 });
+definePageMeta({
+  layout: "custom",
+});
 </script>
 
 <template>
-  <div
-    class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5"
-  >
-    <!-- cars page -->
-    <div class="flex mt-32">
-      <CarSideBar />
-      <NuxtPage />
-    </div>
+  <!-- cars page -->
+  <div class="flex mt-32">
+    <CarSideBar />
+    <NuxtPage />
   </div>
 </template>
