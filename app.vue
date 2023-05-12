0aa4dc2aaab894cf88a6cebbcf0f712fdcd33073
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-13 01:35:17 +1000

    file based routing

--- app.vue
+++ app.vue
@@ -1,11 +1,8 @@
+
+
 <template>
   <div>
-    <!-- navbar -->
-    <header
-      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
-    >
-      <a class="text-3xl font-mono" href="">CarBuy</a>
-    </header>
-   
+    <NavBar/>
+    <NuxtPage/>
   </div>
 </template>
