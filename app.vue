c9bc3391159e1b9e5847d59f6144e57e79682dcd
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-12 23:32:33 +1000

    build the home hero

--- app.vue
+++ app.vue
@@ -1,8 +1,28 @@
 <template>
   <div>
     <!-- navbar -->
-    <header class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md">
+    <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
       <a class="text-3xl font-mono" href="">CarBuy</a>
     </header>
+
+    <!-- home hero -->
+    <div
+      class="relative h-screen w-full bg-no-repeat bg-bottom bg-cover bg-[url('https://mysterio.yahoo.com/mysterio/api/8964DB5E909CACD18049306AFDB356D51F83B94D69A71CF8364374A44354E9D0/autoblog/resizefill_w1062_h597;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/os/ab/_cms/2021/10/12164830/2022-Lexus-LX-600-Ultra-Luxury-action-desert-front.jpg')]"
+    >
+      <!-- -->
+
+      <div class="z-10 absolute w-full h-full flex justify-center items-center">
+        <div class="text-center w-1/2">
+          <h1 class="text-8xl font-serif mb-10 text-white">
+            Find Your Car
+          </h1>
+        </div>
+      </div>
+      <div class="z-1 absolute  w-full h-full bg-black opacity-50">
+
+      </div>
+    </div>
   </div>
 </template>
