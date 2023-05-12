e7ea3f555ea6facadc93944978fa207da77e3992
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-13 02:17:40 +1000

    filing the page with components

---
+++ components/Car/Card.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- car card -->
+  <div
+    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <div class="flex h-full">
+      <img
+        src="https://hips.hearstapps.com/hmg-prod/images/2023-mercedes-benz-g-class-g550-101-1674061257.jpg?crop=0.578xw:0.433xh;0.362xw,0.306xh&resize=640:*"
+        alt=""
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">g-wagon</h1>
+          <p class="text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <h1 class="text-xl mt-auto">$39,000</h1>
+      </div>
+    </div>
+  </div>
+</template>
