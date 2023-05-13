12ac488d89963eb40e34b1624fd7446993377524
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-14 02:40:52 +1000

    nuxterror boundary in handling client side errors

--- pages/city/[city]/car.vue
+++ pages/city/[city]/car.vue
@@ -14,7 +14,21 @@ definePageMeta({
 <template>
   <!-- cars page -->
   <div class="flex mt-32">
-    <CarSideBar />
-    <NuxtPage />
+    <NuxtErrorBoundary>
+      <CarSideBar />
+      <NuxtPage />
+      <template #error="{ error }">
+        <div class="text-center mx-auto flex flex-col">
+          <h1 class="text-5xl text-red-600 mb-4">Something went wrong</h1>
+          <code>{{ error }}</code>
+          <button
+            class="text-white bg-blue-400 px-10 py-3 rounded mt-4"
+            @click="error.value = null"
+          >
+            Go Back
+          </button>
+        </div>
+      </template>
+    </NuxtErrorBoundary>
   </div>
 </template>
