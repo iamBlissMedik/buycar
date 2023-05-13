9d7a6abaabb106e96c7b5595e09d897614daacec
Ellams Bliss <ellamsbliss@gmail.com>
2023-05-13 22:13:07 +1000

    rendering the cards dynamically

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -1,22 +1,24 @@
+<script setup>
+const props = defineProps({
+  car: Object,
+});
+</script>
+
 <template>
   <!-- car card -->
   <div
-    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]" @click="navigateTo(`/car/${car.name}-${car.id}`)"
   >
     <div class="flex h-full">
-      <img
-        src="https://hips.hearstapps.com/hmg-prod/images/2023-mercedes-benz-g-class-g550-101-1674061257.jpg?crop=0.578xw:0.433xh;0.362xw,0.306xh&resize=640:*"
-        alt=""
-        class="w-[300px] h-full"
-      />
+      <img :src="car.url" :alt="car.name" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
-          <h1 class="text-2xl text-blue-700">g-wagon</h1>
+          <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
           <p class="text-gray-700">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            {{ car.description }}
           </p>
         </div>
-        <h1 class="text-xl mt-auto">$39,000</h1>
+        <h1 class="text-xl mt-auto">${{ car.price }}</h1>
       </div>
     </div>
   </div>
